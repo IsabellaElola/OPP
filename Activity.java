50a82016b834fd2931f80651c6c530c581bf03c8
IsabellaElola <isabellagraceelola.gmail.com>
2024-09-20 16:01:06 +0800

    first commit

---
+++ Activity.java
@@ -0,0 +1,18 @@
+class Activity{
+
+public static void main(String[] args){ 
+
+    Dog dog = new Dog(); 
+    dog.setName(dogName:"Panpan"); 
+
+    system.out.printIn(dog.showName());
+    system.out.printIn(dog.bark());
+}
+
+Dog dog = new Dog(); 
+dog.setName(dogName:"Enchoy"); 
+
+system.out.printIn(dog.showName());
+system.out.printIn(dog.bark());
+}
+
